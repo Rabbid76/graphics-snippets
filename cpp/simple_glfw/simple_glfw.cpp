d1dbe48979cecf3b482da65f1dbac7fd28d76e26
Rabbid76 <Gernot.Steinegger@gmail.com>
2018-04-16 19:35:19 +0200

    fix

--- cpp/simple_glfw/simple_glfw.cpp
+++ cpp/simple_glfw/simple_glfw.cpp
@@ -134,7 +134,6 @@ void CWindow_Glfw::Init( int width, int height, int multisampling, bool doubleBu
     glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
     glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 6 );
     glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
-    glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
     glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE );
 #if defined(_DEBUG)
     glfwWindowHint( GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE );
