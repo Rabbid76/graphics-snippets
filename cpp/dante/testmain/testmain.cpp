f3ff3361066a6885204e9188f4b139d3c07d2074
Rabbid76 <Gernot.Steinegger@gmail.com>
2019-02-12 20:52:21 +0100

    sync

--- cpp/dante/testmain/testmain.cpp
+++ cpp/dante/testmain/testmain.cpp
@@ -213,21 +213,95 @@ void main()
 
 
 std::string sh_perlin_noise = R"(
-#version 400 core
+#version 460 core
 
 in vec2 vUV;
 
 out vec4 fragColor;
 
+uniform float u_time_ms;
+
+int permutation[256] = {
+    151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225,
+    140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6, 148,
+    247, 120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32,
+    57, 177, 33, 88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175,
+    74, 165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231, 83, 111, 229, 122,
+    60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244, 102, 143, 54,
+    65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169,
+    200, 196, 135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64,
+    52, 217, 226, 250, 124, 123, 5, 202, 38, 147, 118, 126, 255, 82, 85, 212,
+    207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42, 223, 183, 170, 213,
+    119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 172, 9,
+    129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 104,
+    218, 246, 97, 228, 251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241,
+    81, 51, 145, 235, 249, 14, 239, 107, 49, 192, 214, 31, 181, 199, 106, 157,
+    184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254, 138, 236, 205, 93,
+    222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180,
+};
+
+int perm(int i) { return permutation[i % 256]; }
+
+vec3 fade(vec3 t) { return t * t * t * (t * (t * 6.0 - 15.0) + 10.0); }
+
+float grad(int hash, float x, float y, float z)
+{
+    int h = hash & 15;                      
+    float u = h<8 ? x : y,                 
+          v = h<4 ? y : h==12||h==14 ? x : z;
+    return ((h&1) == 0 ? u : -u) + ((h&2) == 0 ? v : -v);
+}
+
+float noise(in vec3 p)
+{
+    vec3  p256 = fract( abs(p) ) * 255.0;
+    ivec3 i256 = ivec3( floor(p256) );
+    vec3  pf   = fract( p256 );   
+    vec3  uvw  = fade( pf.xyz );
+    
+    int A  = perm(i256.x  ) + i256.y;
+    int B  = perm(i256.x+1) + i256.y;
+    int AA = perm(A)   + i256.z;
+    int BA = perm(B)   + i256.z;
+    int AB = perm(A+1) + i256.z;      
+    int BB = perm(B+1) + i256.z;   
+ 
+    return mix(uvw.z, mix(uvw.y, mix(uvw.x, grad( perm(AA  ), pf.x  , pf.y  , pf.z   ), 
+                                            grad( perm(BA  ), pf.x-1, pf.y  , pf.z   )),
+                                 mix(uvw.x, grad( perm(AB  ), pf.x  , pf.y-1, pf.z   ), 
+                                            grad( perm(BB  ), pf.x-1, pf.y-1, pf.z   ))),
+                      mix(uvw.y, mix(uvw.x, grad( perm(AA+1), pf.x  , pf.y  , pf.z-1 ), 
+                                            grad( perm(BA+1), pf.x-1, pf.y  , pf.z-1 )), 
+                                 mix(uvw.x, grad( perm(AB+1), pf.x  , pf.y-1, pf.z-1 ),
+                                            grad( perm(BB+1), pf.x-1, pf.y-1, pf.z-1 ))));
+}
+
+vec3 HUEtoRGB(in float H)
+{
+    float R = abs(H * 6.0 - 3.0) - 1.0;
+    float G = 2.0 - abs(H * 6.0 - 2.0);
+    float B = 2.0 - abs(H * 6.0 - 4.0);
+    return clamp( vec3(R,G,B), 0.0, 1.0 );
+}
+
 void main()
 {
-    fragColor = vec4(vUV.x, vUV.y, (1.0 - vUV.x)*(1.0 - vUV.y), 1.0);
+    vec2 offs = vec2(u_time_ms / 5000.0, u_time_ms / 15000.0);
+    //offs = vec2(0.0);
+
+    float n = noise(vec3(vUV.xy + offs.xy, 0.0));
+    //float n = noise(vec3(vUV.xy + offs.xy, (1.0 - vUV.x)*(1.0 - vUV.y)));
+
+    fragColor = vec4( HUEtoRGB( n ), 1.0);
+    
+    //fragColor = vec4(vUV.x, vUV.y, (1.0 - vUV.x)*(1.0 - vUV.y), 1.0);
 }
 )";
 
 
 GLint proj_loc  = -1;
 GLint model_loc = -1;
+GLint time_loc  = -1;
 GLuint program = 0;
 
 void CWindow_Glfw::InitScene( void )
@@ -276,6 +350,7 @@ void CWindow_Glfw::InitScene( void )
 
     proj_loc  = glGetUniformLocation( program, "u_proj" );
     model_loc = glGetUniformLocation( program, "u_model" );
+    time_loc  = glGetUniformLocation( program, "u_time_ms" );
 
     glUseProgram( program );
 }
@@ -291,6 +366,8 @@ void CWindow_Glfw::Render( double time_ms )
 
     glUniformMatrix4fv( proj_loc,  1, GL_FALSE, glm::value_ptr( proj ) );
     glUniformMatrix4fv( model_loc, 1, GL_FALSE, glm::value_ptr( model ) );
+    glUniform1f( time_loc, time_ms );
+
 
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);  
     glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
