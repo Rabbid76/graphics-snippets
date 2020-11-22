bdc3c444f0622a2110c023e1f506d723974213f9
Rabbid76 <Gernot.Steinegger@gmail.com>
2020-11-22 16:39:41 +0100

    OpenGL Visual Studio Project

---
+++ example/cpp/opengl/opengl_example.cpp
@@ -0,0 +1,14 @@
+
+#define main example_camera_and_matrix
+#include <example_camera_and_matrix.cpp>
+#undef main
+
+int main()
+{
+    int ret = 0;
+
+    ret = example_camera_and_matrix();
+
+    return ret;
+}
+
