6787d577c01142e683352c8a5efa11f366edaec5
Rabbid76 <Gernot.Steinegger@gmail.com>
2022-01-17 20:18:11 +0100

    opengl example

---
+++ example/cpp/windows_opengl/example_wgl_1.cpp
@@ -0,0 +1,186 @@
+#include <stdafx.h>
+
+#include <windows.h>
+#include <GL/glew.h>
+#include <GL/gl.h>
+
+#include <vector>
+#include <stdexcept>
+#include <iostream>
+
+class shadoeGE
+{
+public:
+
+    static LRESULT CALLBACK WindowProcedure(HWND hWnd, unsigned int msg, WPARAM wparam, LPARAM lparam);
+    HWND Init(int width, int height);
+    void DestroyWindow(void);
+    void MessageLoop(void);
+    void Display(void);
+
+private:
+
+    HWND hOGLWnd = NULL;
+    HGLRC hOGLRenderContext = NULL;
+};
+
+std::wstring wnd_class(L"my_wnd_class");
+shadoeGE wnd;
+
+int main()
+{
+    int w = 800;
+    int h = 600;
+
+    HWND hWnd = wnd.Init(w, h);
+    if (hWnd == 0)
+        throw std::runtime_error("error initializing window");
+
+    wnd.MessageLoop();
+    wnd.DestroyWindow();
+    return 0;
+}
+
+HWND shadoeGE::Init(int width, int height)
+{
+    // Get module handle
+    HMODULE hModule = ::GetModuleHandle(0);
+    if (!hModule)
+        return NULL;
+
+    // Create window class
+    WNDCLASSEX wndClassData;
+    memset(&wndClassData, 0, sizeof(WNDCLASSEX));
+    wndClassData.cbSize = sizeof(WNDCLASSEX);
+    wndClassData.style = CS_DBLCLKS;
+    wndClassData.lpfnWndProc = WindowProcedure;
+    wndClassData.cbClsExtra = 0;
+    wndClassData.cbWndExtra = 0;
+    wndClassData.hInstance = hModule;
+    wndClassData.hIcon = ::LoadIcon(0, IDI_APPLICATION);
+    wndClassData.hCursor = ::LoadCursor(0, IDC_ARROW);
+    wndClassData.hbrBackground = ::CreateSolidBrush(COLOR_WINDOW + 1);
+    wndClassData.lpszMenuName = 0;
+    wndClassData.lpszClassName = wnd_class.c_str();
+    wndClassData.hIconSm = 0;
+    if (!::RegisterClassEx(&wndClassData))
+        return NULL;
+
+    // Creaate Window
+    hOGLWnd = ::CreateWindow(wnd_class.c_str(), NULL, WS_OVERLAPPEDWINDOW, 0, 0, width, height, NULL, NULL, hModule, NULL);
+    if (hOGLWnd == NULL)
+        return NULL;
+
+    // Get device context
+    HDC hDC = ::GetDC(hOGLWnd);
+
+    // Create OpenGL context
+    DWORD pixelFormatFlags = PFD_SUPPORT_OPENGL | PFD_SUPPORT_COMPOSITION | PFD_GENERIC_ACCELERATED | PFD_DRAW_TO_WINDOW | PFD_DOUBLEBUFFER;
+    PIXELFORMATDESCRIPTOR pfd =
+    {
+      sizeof(PIXELFORMATDESCRIPTOR),
+      1,
+      pixelFormatFlags,         //Flags
+      PFD_TYPE_RGBA,            //The kind of framebuffer. RGBA or palette.
+      32,                       //Colordepth of the framebuffer.
+      0, 0, 0, 0, 0, 0,
+      0,
+      0,
+      0,
+      0, 0, 0, 0,
+      24,                        //Number of bits for the depthbuffer
+      8,                        //Number of bits for the stencilbuffer
+      0,                        //Number of Aux buffers in the framebuffer.
+      PFD_MAIN_PLANE,
+      0,
+      0, 0, 0
+    };
+    int pixelFormat = ::ChoosePixelFormat(hDC, &pfd);
+    ::SetPixelFormat(hDC, pixelFormat, &pfd);
+    hOGLRenderContext = ::wglCreateContext(hDC);
+
+    // make OpenGL context the current context
+    ::wglMakeCurrent(hDC, hOGLRenderContext);
+
+    if (glewInit() != GLEW_OK)
+        throw std::runtime_error("error initializing glew");
+
+    int major, minor;
+    glGetIntegerv(GL_MAJOR_VERSION, &major);
+    glGetIntegerv(GL_MINOR_VERSION, &minor);
+    std::cout << glGetString(GL_VERSION) << " - " << major << "." << minor << std::endl;
+
+    // release device context
+    ::ReleaseDC(hOGLWnd, hDC);
+
+    // show the window
+    ::ShowWindow(hOGLWnd, SW_SHOWDEFAULT);
+    return hOGLWnd;
+}
+
+void shadoeGE::MessageLoop(void)
+{
+    MSG msg;
+    while (::GetMessage(&msg, 0, 0, 0))
+        ::DispatchMessage(&msg);
+}
+
+void shadoeGE::DestroyWindow(void)
+{
+    ::DestroyWindow(hOGLWnd);
+    ::wglMakeCurrent(NULL, NULL);
+    ::wglDeleteContext(hOGLRenderContext);
+
+    HMODULE hModule = ::GetModuleHandle(0);
+    if (!hModule)
+        return;
+    ::UnregisterClass(wnd_class.c_str(), hModule);
+}
+
+LRESULT CALLBACK shadoeGE::WindowProcedure(HWND hWnd, unsigned int msg, WPARAM wparam, LPARAM lparam)
+{
+    switch (msg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+    case WM_PAINT:
+        wnd.Display();
+        break;
+    }
+    return DefWindowProc(hWnd, msg, wparam, lparam);
+}
+
+void shadoeGE::Display(void)
+{
+    RECT clientRect;
+    ::GetClientRect(hOGLWnd, &clientRect);
+
+    glViewport(0, 0, clientRect.right - clientRect.left, clientRect.bottom - clientRect.top);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+
+    glColor3f(0.1, 0.2, 0.3);
+    glVertex3f(0, 0, 0);
+    glVertex3f(1, 0, 0);
+    glVertex3f(0, 1, 0);
+
+    glEnd();
+
+    glColor3f(1.0f, 0.0f, 0.0f);
+
+    glEnableVertexAttribArray(0);
+    GLfloat verts[] = { 0.5, 0.0, 0.0, -0.5, 0.0, 0.0, 0.0, 0.5, 0.0 };
+    glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, verts);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glDisableVertexAttribArray(0);
+
+    HDC hDC = ::GetDC(hOGLWnd);
+    ::SwapBuffers(hDC);
+    ::ReleaseDC(hOGLWnd, hDC);
+}
---
+++ example/cpp/windows_opengl/example_wglew_1.cpp
@@ -0,0 +1,217 @@
+#include <stdafx.h>
+
+#include <GL/glew.h>
+#include <GL/wglew.h>
+
+#include <iostream>
+#include <string>
+
+#include <windows.h>
+
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void display(void);
+
+std::string errorStr = "none";
+
+int InitOpengl(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR     lpCmdLine,
+    int       nCmdShow,
+    HWND& hwnd,
+    HDC& hdc,
+    HGLRC& hrc)
+{
+    //---- fake Window
+    WNDCLASSEX wcex;
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = L"test_class";
+    wcex.hIconSm = NULL;
+
+    if (!RegisterClassEx(&wcex))
+    {
+        errorStr = "RegisterClassEx";
+        return 0;
+    }
+
+    PIXELFORMATDESCRIPTOR pfd;
+    memset(&pfd, 0, sizeof(PIXELFORMATDESCRIPTOR));
+    pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+    pfd.dwFlags = PFD_DOUBLEBUFFER | PFD_SUPPORT_OPENGL | PFD_DRAW_TO_WINDOW;
+    pfd.iPixelType = PFD_TYPE_RGBA;
+    pfd.cColorBits = 32;
+    pfd.cDepthBits = 32;
+    pfd.iLayerType = PFD_MAIN_PLANE;
+
+    HWND temp_hwnd = CreateWindow(
+        L"test_class",
+        L"test",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        500, 500,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
+
+    HDC temp_hdc = GetDC(temp_hwnd);
+
+    int temp_nPixelFormat = ChoosePixelFormat(temp_hdc, &pfd);
+    SetPixelFormat(temp_hdc, temp_nPixelFormat, &pfd);
+
+    HGLRC temp_hrc = wglCreateContext(temp_hdc);
+
+    wglMakeCurrent(temp_hdc, temp_hrc);
+
+    glewExperimental = true;
+    glewInit();
+
+    bool wg_support = wglewIsSupported("WGL_ARB_create_context") == 1;
+
+    if (wg_support == false)
+    {
+        hwnd = temp_hwnd;
+        hdc = temp_hdc;
+        hrc = temp_hrc;
+        errorStr = "WGL_ARB_create_context";
+        return 0;
+    }
+
+    // OpenGL context window
+
+    wglMakeCurrent(NULL, NULL);
+    wglDeleteContext(temp_hrc);
+    ReleaseDC(temp_hwnd, temp_hdc);
+    DestroyWindow(temp_hwnd);
+
+    hwnd = CreateWindow(
+        L"test_class",
+        L"OpenGL context window",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        500, 500,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
+
+    hdc = GetDC(hwnd);
+
+    const int iPixelFormatAttribList[] = {
+        WGL_DRAW_TO_WINDOW_ARB, GL_TRUE,
+        WGL_SUPPORT_OPENGL_ARB, GL_TRUE,
+        WGL_DOUBLE_BUFFER_ARB, GL_TRUE,
+        WGL_PIXEL_TYPE_ARB, WGL_TYPE_RGBA_ARB,
+        WGL_COLOR_BITS_ARB, 32,
+        WGL_DEPTH_BITS_ARB, 24,
+        WGL_STENCIL_BITS_ARB, 8,
+        0 // End of attributes list
+    };
+    int attributes[] = {
+        WGL_CONTEXT_MAJOR_VERSION_ARB, 4,
+        WGL_CONTEXT_MINOR_VERSION_ARB, 6,
+        WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB,
+        WGL_CONTEXT_FLAGS_ARB, WGL_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB,
+        0
+    };
+
+    int nPixelFormat = 0;
+    UINT iNumFormats = 0;
+
+    wglChoosePixelFormatARB(hdc, iPixelFormatAttribList, NULL, 1, &nPixelFormat, (UINT*)&iNumFormats);
+
+    SetPixelFormat(hdc, nPixelFormat, &pfd);
+
+    hrc = wglCreateContextAttribsARB(hdc, 0, attributes);
+
+    wglMakeCurrent(NULL, NULL);
+    wglMakeCurrent(hdc, hrc);
+
+    return 1;
+}
+
+
+bool progRun = false;
+int CALLBACK WinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR     lpCmdLine,
+    int       nCmdShow
+)
+{
+    HWND hwnd = NULL;
+    HDC  hdc = NULL;
+    HGLRC hrc = NULL;
+    InitOpengl(hInstance, hPrevInstance, lpCmdLine, nCmdShow, hwnd, hdc, hrc);
+
+    ShowWindow(hwnd, SW_SHOW);
+
+    glClearColor(0, 0, 0, 1);
+
+    MSG msg;
+
+    progRun = true;
+    while (progRun)
+    {
+
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        glViewport(0, 0, 500, 500);
+        display();
+        GLint errorCode = glGetError();
+        if (errorCode != GL_NO_ERROR)
+        {
+            std::cout << "error: " << errorCode << std::endl;
+        }
+
+        SwapBuffers(hdc);
+
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_CREATE:
+        // CreateContext( hWnd );
+        break;
+
+    case WM_DESTROY:
+        //DestroyContext( hWnd );
+        PostQuitMessage(0);  // TODO $$$ close and destroy window ???
+        progRun = false;
+        break;
+    }
+
+    return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+void display(void)
+{
+    glLineWidth(5.0);
+    glColor4f(1, 1, 1, 1);
+    glBegin(GL_LINES);
+    glVertex2f(-0.8, -0.8);
+    glVertex2f(0.8, 0.8);
+    glEnd();
+}
